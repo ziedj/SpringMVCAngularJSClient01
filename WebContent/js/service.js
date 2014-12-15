90269e61161c16c02cd9563d25d6d2eb9da44b59
Zied Jabnoun <zied.jab@gmail.com>
2014-12-15 15:38:31 +0100

    / ==> /rest/*

--- WebContent/js/service.js
+++ WebContent/js/service.js
@@ -1,16 +1,18 @@
 /**
  * 
  */
-angular.module('myApp.services', [ 'ngResource' ]).factory(
-		'AngularIssues',
-		function($resource) {
-			return $resource(
-					'http://localhost:8080/SpringMVCAngularJSRESTAndClient01/newUser',
-					{}, {
-						query : {
-							method : 'GET',
-							params : {},
-							isArray : false
-						}
-					})
-		})
+angular
+		.module('myApp.services', [ 'ngResource' ])
+		.factory(
+				'AngularIssues',
+				function($resource) {
+					return $resource(
+							'http://localhost:8080/SpringMVCAngularJSRESTAndClient01/rest/newJsonUser',
+							{}, {
+								query : {
+									method : 'GET',
+									params : {},
+									isArray : false
+								}
+							})
+				})
